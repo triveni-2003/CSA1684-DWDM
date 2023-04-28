06c4cfc0831cadd63da750f08bd65d17efba14c9
triveni-2003 <115334273+triveni-2003@users.noreply.github.com>
2023-04-28 20:38:58 +0530

    Create finding mean of sq.r

---
+++ finding mean of sq.r
@@ -0,0 +1,6 @@
+print("sequence of numbers from 20 to 50:")
+print(seq(20,50))
+print("sequence of numbers from 20 to 60:")
+print(seq(20:60))
+print("sequence of numbers from 51 to 91:")
+print(seq(51:91))
